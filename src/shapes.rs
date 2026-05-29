257c014d5c805c83c1970884b39669ee75bdb3ac
Sergey Moshnikov <s.moshnikov@gmail.com>
2026-05-29 11:13:22 +0200

    Removed flat_sphere

--- src/shapes.rs
+++ src/shapes.rs
@@ -2,10 +2,8 @@
 
 mod cube;
 mod dodecahedron;
-mod flat_sphere;
 mod sphere;
 
 pub use cube::cube;
 pub use dodecahedron::dodecahedron;
-pub use flat_sphere::sphere as flat_sphere;
 pub use sphere::sphere;
