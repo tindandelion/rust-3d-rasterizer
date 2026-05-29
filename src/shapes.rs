698e5b2aa26b18d7bc15054107e4a38990a7de9e
Sergey Moshnikov <s.moshnikov@gmail.com>
2026-05-29 11:02:36 +0200

    Added flat sphere for testing

--- src/shapes.rs
+++ src/shapes.rs
@@ -2,8 +2,10 @@
 
 mod cube;
 mod dodecahedron;
+mod flat_sphere;
 mod sphere;
 
 pub use cube::cube;
 pub use dodecahedron::dodecahedron;
+pub use flat_sphere::sphere as flat_sphere;
 pub use sphere::sphere;
