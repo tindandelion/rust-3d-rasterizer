a881f19bcf654b71fdaebdd803fb790f5fc7ad35
Sergey Moshnikov <s.moshnikov@gmail.com>
2026-06-09 09:35:03 +0200

    Cleaning up

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -46,16 +46,6 @@ impl FrameBuffer {
         self.depth.fill(f32::INFINITY);
     }
 
-    pub fn set_pixel(&mut self, x: u32, y: u32, color: Rgb) {
-        let Some(i) = self.pixel_offset(x, y) else {
-            return;
-        };
-
-        self.rgb[i] = color.0;
-        self.rgb[i + 1] = color.1;
-        self.rgb[i + 2] = color.2;
-    }
-
     /// Writes **color** at **point** when **point.depth** is nearer than the stored depth.
     pub fn write_pixel(&mut self, point: FbPoint, color: Rgb) {
         let Some(i) = self.pixel_index(point.x, point.y) else {
@@ -80,10 +70,6 @@ impl FrameBuffer {
             Some(y as usize * self.width as usize + x as usize)
         }
     }
-
-    fn pixel_offset(&self, x: u32, y: u32) -> Option<usize> {
-        self.pixel_index(x, y).map(|i| i * 3)
-    }
 }
 
 impl AsRef<[u8]> for FrameBuffer {
@@ -100,7 +86,7 @@ mod test_helpers {
         const ASCII_SHADES: [char; 4] = ['░', '▒', '▓', '█'];
 
         pub(crate) fn get_pixel(&self, x: u32, y: u32) -> Rgb {
-            let Some(i) = self.pixel_offset(x, y) else {
+            let Some(i) = self.pixel_index(x, y).map(|i| i * 3) else {
                 return Rgb::BLACK;
             };
             Rgb(self.rgb[i], self.rgb[i + 1], self.rgb[i + 2])
@@ -241,30 +227,6 @@ mod tests {
     }
 
     #[test]
-    fn set_pixel_writes_rgb_at_correct_offset() {
-        let mut fb = FrameBuffer::new(3, 3);
-        fb.set_pixel(1, 1, Rgb::WHITE);
-
-        assert_eq!(fb.get_pixel(1, 1), Rgb::WHITE);
-        assert_eq!(fb.get_pixel(0, 0), Rgb::BLACK);
-
-        #[rustfmt::skip]
-        let expected = [
-            0, 0, 0,            0,            0,            0, 0, 0, 0,
-            0, 0, 0, Rgb::WHITE.0, Rgb::WHITE.1, Rgb::WHITE.2, 0, 0, 0,
-            0, 0, 0,            0,            0,            0, 0, 0, 0,
-        ];
-        assert_eq!(fb.as_ref(), expected.as_slice());
-    }
-
-    #[test]
-    fn set_pixel_out_of_bounds_is_ignored() {
-        let mut fb = FrameBuffer::new(2, 2);
-        fb.set_pixel(100, 0, Rgb::WHITE);
-        assert!(fb.as_ref().iter().all(|&b| b == 0));
-    }
-
-    #[test]
     fn border_ascii_art_draws_box_around_rows() {
         let boxed = test_helpers::border_ascii_art(" █\n██");
         assert_eq!(
@@ -279,8 +241,8 @@ mod tests {
     #[test]
     fn to_ascii_art_maps_black_to_space_and_white_to_full_block() {
         let mut fb = FrameBuffer::new(2, 1);
-        fb.set_pixel(0, 0, Rgb::BLACK);
-        fb.set_pixel(1, 0, Rgb::WHITE);
+        fb.write_pixel(FbPoint::new(0, 0, 0.0), Rgb::BLACK);
+        fb.write_pixel(FbPoint::new(1, 0, 0.0), Rgb::WHITE);
         assert_ascii_art_eq(&fb.to_ascii_art(), &to_ascii_art(&[" █"]), "");
     }
 
